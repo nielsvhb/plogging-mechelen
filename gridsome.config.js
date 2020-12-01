c2cfa33139bf2f526c0b96da7317f583926cdd76
Niels Vanhorenbeeck <nielsvhb@hotmail.com>
2020-12-01 23:22:09 +0100

    Initial commit

---
+++ gridsome.config.js
@@ -0,0 +1,26 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'Gridsome',
+  plugins: [
+    {
+      use: '@gridsome/source-strapi',
+      options: {
+        apiURL: process.env.NODE_ENV === 'development' ? 'http://localhost:1337' : 'https://plogging-mechelen-api.herokuapp.com/',
+        queryLimit: 1000, // Defaults to 100
+        contentTypes: ['user'],
+        singleTypes: ['homepage'],
+        // Possibility to login with a Strapi user,
+        // when content types are not publicly available (optional).
+        loginData: {
+          identifier: '',
+          password: ''
+        }
+      }
+    }
+  ]
+}
---
+++ src/pages/About.vue
@@ -0,0 +1,25 @@
+<template>
+  <Layout>
+    <h1>{{$page.homepage.title}}</h1>
+    <p>{{$page.homepage.mission}}</p>
+  </Layout>
+</template>
+
+<page-query>
+  query {
+    homepage: strapiHomepage(id: 1) {
+      id
+      title: Title
+      mission: Mission
+    }
+}
+
+</page-query>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'About us'
+  }
+}
+</script>
