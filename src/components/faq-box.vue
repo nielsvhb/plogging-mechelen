66805f8c23b8176ed96b3428b76bd68d08ba8527
Niels Vanhorenbeeck <nielsvhb@hotmail.com>
2020-12-16 22:14:14 +0100

    start met ploggen + faq updated

---
+++ src/components/faq-box.vue
@@ -0,0 +1,41 @@
+<template>
+   <li class="mb-6 flex  p-3 border-gray-100 border rounded-md bg-gray-50" >
+        <div class="flex items-center justify-center bg-blue-50 w-8 h-8 text-blue-500 rounded-full absolute">
+            <i class="fas fa-question"></i>
+        </div>
+        <div class="pl-12 pr-4">
+            <div class="text-gray-700 font-semibold cursor-pointer" @click="toggle()">{{question}}</div>
+            <p class="text-gray-600 font-light overflow-hidden" :class="{'h-0': !isOpen, 'h-auto': isOpen}">
+                {{answer}}
+            </p>
+        </div>
+        <div class="ml-auto p-1 cursor-pointer rounded hover:text-blue-200"  @click="toggle()">
+            <i v-if="!isOpen" class="fas fa-angle-down"></i>
+            <i v-else class="fas fa-angle-up"></i>
+        </div>
+    </li>
+</template>
+
+<script>
+    export default {
+        name: 'faq-box',
+        props: {
+            answer: String,
+            question: String
+        },
+        data() {
+            return {
+                isOpen: false
+            }
+        },
+        methods: {
+            toggle() {
+                this.isOpen = !this.isOpen;
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/pages/Faq.vue
+++ src/pages/Faq.vue
@@ -1,20 +1,10 @@
 <template>
 <Layout>
     <div class="mx-auto container py-20">
-        <h1 class="uppercase text-5xl font-bold mb-10">Veelgestelde vragen</h1>
+        <h1 class="uppercase text-5xl font-bold mb-10 text-gray-600">Veelgestelde vragen</h1>
         <p v-if="!faqs.length">Geen veegestelde vragen gevonden.</p>
         <ul>
-            <li v-for="faq in faqs" :key="faq.id" class="mb-10 flex space-x-3">
-                <div class="flex items-center justify-center bg-blue-50 w-8 h-8 text-blue-500 rounded-full absolute">
-                    <i class="fas fa-question"></i>
-                </div>
-                <div class="pl-10">
-                    <div class="text-gray-700 font-semibold">{{faq.question}}</div>
-                    <p class="text-gray-600 font-light">
-                        {{faq.answer}}
-                    </p>
-                </div>
-            </li>
+            <faq-box v-for="faq in faqs" :key="faq.id" :question="faq.question" :answer="faq.answer"></faq-box>
         </ul>
   </div>
   </Layout>
@@ -35,7 +25,9 @@ query {
 
 </page-query>
 <script>
+import faqBox from '../components/faq-box.vue';
 export default {
+  components: { faqBox },
     name: 'faq_page',
     data(){
        return {
