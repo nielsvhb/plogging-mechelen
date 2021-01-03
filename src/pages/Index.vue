7fe9e46df5b5278d4626e4f57890d0df670c73eb
Niels Vanhorenbeeck <nielsvhb@hotmail.com>
2021-01-03 21:13:32 +0100

    Homepage: news added hard-coded

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -9,7 +9,7 @@
         </div>
 
         <div class="mt-24">
-          <h3 class="text-gray-400 uppercase font-semibold">Ploggen in cijfers</h3>
+          <div class="text-gray-400 uppercase font-semibold">Ploggen in cijfers</div>
 
           <div class="mt-4 space-y-2 text-gray-500">
             <div class="text-2xl font-bold">
@@ -30,13 +30,27 @@
       </div>
     </div>
 
+    <div class="mx-auto p-20 bg-gray-50 my-20">
+      <div>
+        <div class="uppercase text-5xl font-bold mb-4 text-gray-600 text-center"><h2>Laatste nieuws</h2></div>
+        <div class="flex">
+          <div v-for="(article, index) in articles" :key="index" class="mx-8 rounded-xl p-10 shadow bg-white" :class="getBgClass(index)">
+            <div class="flex items-center mb-5 "><i class="fas fa-bullhorn mr-2" :class="getTextClass(index)"></i><h3 class="font-bold text-xl tracking-wide" :class="getTextClass(index)">{{article.title}}</h3></div>
+            <p class="text-gray-600">
+              {{article.content}}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <div class="mx-auto container relative">
       <div class=" overflow-auto rounded-2xl shadow-2xl absolute left-0 top-8" >
           <g-image src="~/assets/plogging1.jpg" width="580" class="block"/>
       </div>
       <div class="py-40 w-1/3 z-20 mx-auto relative">
         <div class="rounded-2xl bg-gray-50 p-8  shadow-2xl">
-          <h2 class="uppercase mb-4 font-bold">{{$page.homepage.InitiativeTitle}}</h2>
+          <h2 class="uppercase font-bold mb-4 text-gray-600">{{$page.homepage.InitiativeTitle}}</h2>
           <p class="text-gray-700 font-light">
             {{$page.homepage.InitiativeText}}
           </p>
@@ -77,7 +91,21 @@ export default {
   data() {
     return {
         tweenedDistance: 0,
-        tweenedWeight: 0
+        tweenedWeight: 0,
+        articles: [
+          {
+            title: 'Snelrechtprocedure',
+            content: 'Momenteel bekijken we of het mogelijk is om jonge mensen, die door snelrechtprocedure voor kleine feiten een dienstverlening willen doen, kunnen inschakelen voor het ploggen te Mechelen. Dit is een hart onder de riem van alle ploggers die op sommige plaatsen merken dat het resultaat van hun vrijwilligerswerk af en toe van korte duur is. De Staat denkt en werkt mee en ook als het gaat over repressie en educatie en dat juichen we toe! Samen voor een nog properder Mechelen.'
+          },
+          {
+            title: 'Samenwerking met Stad Mechelen',
+            content: 'Momenteel zijn er gesprekken tussen de app ontwikkelaars van We Plog, Plogging Mechelen en Stad Mechelen. Het zou mooi zijn mocht Stad Mechelen dit burgerinitiatief steunen en het beheer van de app in handen nemen op het Mechels grondgebied. Op deze manier kunnen we beter inspelen op de hotspots waar er regelmatiger moet geplogd worden. Dit betekent concreet dat Stad Mechelen bepaalde straten opnieuw sneller kan ROOD kleuren zodat het voor iedereen zichtbaar wordt op de app. Ook zou het goed zijn dat de stadsdiensten met dezelfde app zouden werken want op die manier ziet iedereen nog beter waar er dient schoongemaakt te worden. Kortom Mechelen zou nog efficiënter kunnen schoongemaakt worden als Stad Mechelen de app beheert.'
+          },
+          {
+            title:'Peters en meters',
+            content: 'We bekijken op dit ogenblik met Stad Mechelen of de 150 Mechelse vrijwilligers ‘Peters en Meters’, die momenteel via de organisatie Mooimakers een stuk straat hebben geclaimd en zich over de properheid ontfermen van hun geclaimd stuk, ook op de hoogte kunnen worden gebracht van de app WePlog. Als deze Mechelaars de app zullen gebruiken wordt ook hun werk zichtbaar en ploggen we met z’n allen nog efficiënter.'
+          }
+        ]
     };
   },
   computed: {
@@ -86,6 +114,30 @@ export default {
     },
     distance() {
         return this.tweenedDistance.toFixed(0);
+    }
+  },
+  methods:{
+    getBgClass(index) {
+      if(index === 0) {
+        return 'bg-pink-50'
+      }
+      if(index === 1) {
+        return 'bg-teal-50'
+      }
+      if(index === 2) {
+        return 'bg-blue-50'
+      }
+    },
+    getTextClass(index) {
+      if(index === 0) {
+        return 'text-pink-600'
+      }
+      if(index === 1) {
+        return 'text-teal-600'
+      }
+      if(index === 2) {
+        return 'text-blue-600'
+      }
     }
   },
   mounted() {
